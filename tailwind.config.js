4a6ed830c2c1b7b2b2b2c51f1c43d35b4046d335
kunj-pandya <kunjpandya998@gmail.com>
2026-03-10 17:38:25 +0530

    [update]: tailwindconfig with global variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg': '#FAF8ED',
+        'brand-base': '#2D3134',
+        'accent': '#F66F4D',
+        'color-text': '#5B5F62',
+      },
+      fontFamily: {
+        display: ['Sen', 'sans-serif'],
+        ui: ['Poppins', 'sans-serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+    container: {
+      center: true,
+      padding: '20px',
+      screens: {
+        '2xl': '1200px',
+      },
+    },
   },
   plugins: [],
 }
