716861891c2605dacd7d231653dbb0dc0ad56ea7
kunj-pandya <kunjpandya998@gmail.com>
2026-03-11 19:34:35 +0530

    update: tailwind config for variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
-        'bg': '#FAF8ED',
-        'brand-base': '#2D3134',
-        'accent': '#F66F4D',
-        'color-text': '#5B5F62',
+        bg: "#FAF8ED",
+        brand: "#2D3134",
+        accent: "#F66F4D",
+        text: "#5B5F62",
+        white: "#ffffff",
+        nav: "#2E476B"
       },
-      fontFamily: {
-        display: ['Sen', 'sans-serif'],
-        ui: ['Poppins', 'sans-serif'],
-        body: ['Inter', 'sans-serif'],
-      },
-    },
-    container: {
-      center: true,
-      padding: '20px',
-      screens: {
-        '2xl': '1200px',
-      },
-    },
-  },
-  plugins: [],
-}
 
+      fontFamily: {
+        display: ["Sen", "sans-serif"],
+        ui: ["Poppins", "sans-serif"],
+        body: ["Inter", "sans-serif"]
+      },
+
+      maxWidth: {
+        container: "1200px"
+      }
+    }
+  },
+  plugins: []
+}
